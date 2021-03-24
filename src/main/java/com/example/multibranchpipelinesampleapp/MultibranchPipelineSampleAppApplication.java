bab92e83902c8f40a922b4336de7cce9ee47cd6c
ElsonRenatoDeFranca <elson.renato.de.franca@gmail.com>
2021-03-24 00:45:19 -0300

    initial commit

---
+++ src/main/java/com/example/multibranchpipelinesampleapp/MultibranchPipelineSampleAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.multibranchpipelinesampleapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultibranchPipelineSampleAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultibranchPipelineSampleAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/multibranchpipelinesampleapp/MultibranchPipelineSampleAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.multibranchpipelinesampleapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultibranchPipelineSampleAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
